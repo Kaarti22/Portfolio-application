d3e263ab749d695e8bc4605884bafbd7db67f786
Kaarti22 <kaartimondi@gmail.com>
2024-08-30 23:29:52 +0530

    created a layout like structure

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,39 @@ import InterviewExperiences from "./pages/InterviewExperiences";
 import Skills from "./pages/Skills";
 import VolunteerExperiences from "./pages/VolunteerExperiences";
 import Experiences from "./pages/Experiences";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/projects" element={<Projects/>}/>
-        <Route path="/achievements" element={<Achievements/>}/>
-        <Route path="/coding-profiles" element={<CodingProfiles/>}/>
-        <Route path="/connect" element={<Connect/>}/>
-        <Route path="/interview-experiences" element={<InterviewExperiences/>}/>
-        <Route path="/skills" element={<Skills/>}/>
-        <Route path="/volunteer-experiences" element={<VolunteerExperiences/>}/>
-        <Route path="/experiences" element={<Experiences/>}/>
-      </Routes>
-    </Router>
+    <div className="min-h-screen bg-gradient-to-b from-indigo-300 to-purple-200">
+      <Router>
+        <div className="min-h-screen max-w-md sm:max-w-lg md:max-w-2xl xl:max-w-4xl mx-auto">
+          <Header />
+          <div className="my-4">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/achievements" element={<Achievements />} />
+              <Route path="/coding-profiles" element={<CodingProfiles />} />
+              <Route path="/connect" element={<Connect />} />
+              <Route
+                path="/interview-experiences"
+                element={<InterviewExperiences />}
+              />
+              <Route path="/skills" element={<Skills />} />
+              <Route
+                path="/volunteer-experiences"
+                element={<VolunteerExperiences />}
+              />
+              <Route path="/experiences" element={<Experiences />} />
+            </Routes>
+          </div>
+          <Footer/>
+        </div>
+      </Router>
+    </div>
   );
 };
 
