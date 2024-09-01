8895dc6f94ca82a03d4739ef32d9e9dc781d3d43
Kaarti22 <kaartimondi@gmail.com>
2024-09-01 12:13:46 +0530

    projects display in home page

--- src/components/ProjectCrousel.tsx
+++ src/components/ProjectCrousel.tsx
@@ -1,4 +1,7 @@
-import { Card, CardContent } from "./ui/card";
+"use client";
+
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
 import {
   Carousel,
   CarouselContent,
@@ -9,9 +12,10 @@ import {
 
 const Projects = [
   {
-    img: "",
+    img: "./AI_mock_interviewer.png",
     title: "Prep AI - An AI Mock Interviewer",
-    description: "Prepare for your upcoming interviews with Prep AI by generating interview questions through Gemini AI, also generating a rating and feedback from your interview.",
+    description:
+      "Prepare for your upcoming interviews with Prep AI by generating interview questions through Gemini AI, also generating a rating and feedback from your interview.",
     skills: [
       "Gemini AI",
       "Next.JS",
@@ -23,47 +27,72 @@ const Projects = [
     ],
   },
   {
-    img: "",
-    title: "",
-    description: "",
+    img: "./FoodByte.png",
+    title: "FoodByte - An Online Food Ordering Website",
+    description:
+      "A 3-level hierarchical platform where users can order food online, restaurant owners can manage customer orders, and admins can manage all the restaurant owners.",
+    skills: ["Next.JS", "MongoDB", "Tailwind CSS", "Stripe", "Google OAuth"],
+  },
+  {
+    img: "./portfolio.png",
+    title: "ViewPoint - An admin manageable Portfolio application",
+    description:
+      "A complete portfolio application with admin panel with a robust backend and two different frontends.",
     skills: [
-      "",
-      "",
+      "React.JS",
+      "Node.JS",
+      "Express.JS",
+      "MongoDB",
+      "Redux Toolkit",
+      "Tailwind CSS",
+      "ShadCn UI",
     ],
   },
   {
-    img: "",
-    title: "",
-    description: "",
-    skills: [
-      "",
-      "",
-    ],
-  },
-  {
-    img: "",
-    title: "",
-    description: "",
-    skills: [
-      "",
-      "",
-    ],
+    img: "./quality_inpection.png",
+    title: "Quality Inspection of Sour Lemons using Deep Learning",
+    description:
+      "A deep learning model of EfficientNet B0 to inspect the quality of sour lemons using image processing technique.",
+    skills: ["Tensorflow", "Scikit-learn", "Keras"],
   },
 ];
 
 const ProjectCrousel = () => {
+  const [currIndex, setCurrIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrIndex((prevIndex) => prevIndex === Projects.length - 1 ? 0 : prevIndex + 1);
+    }, 4000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="flex items-center justify-center">
-      <Carousel className="w-full max-w-3xl">
+      <Carousel className="w-full max-w-sm sm:max-w-md md:max-w-2xl lg:max-w-3xl xl:max-w-4xl" tabIndex={currIndex}>
         <CarouselContent>
           {Projects.map((item, index) => (
             <CarouselItem key={index}>
-              <div className="p-1">
-                <Card>
-                  <CardContent className="h-96 flex items-center justify-center p-6">
-                    <span className="text-4xl font-semibold">{item.title}</span>
-                  </CardContent>
-                </Card>
+              <div className="h-auto md:h-96 grid grid-cols-1 md:grid-cols-5 gap-6 rounded-2xl">
+                <div className="md:col-span-3">
+                  <img
+                    src={`${item.img}`}
+                    alt="AI Mock Interviewer image"
+                    className="w-full h-full object-cover rounded-2xl"
+                  />
+                </div>
+                <div className="md:col-span-2 flex flex-col gap-4">
+                  <div className="text-2xl font-semibold">{item.title}</div>
+                  <div className="text-lg text-gray-500">{item.description}</div>
+                  <Button>Know more</Button>
+                  <div>
+                    {item.skills.map((skill, index) => (
+                      <Button key={index} variant={"outline"} className="h-8 rounded-full text-gray-500">
+                        {skill}
+                      </Button>
+                    ))}
+                  </div>
+                </div>
               </div>
             </CarouselItem>
           ))}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,12 +5,12 @@ const Home = () => {
   return (
     <div className="my-10 flex flex-col gap-10">
       <HeroBanner />
+      <div className="flex flex-col gap-4">
+        <div className="text-4xl font-semibold bg-gradient-to-r from-blue-600 via-green-500 to-pink-500 inline-block text-transparent bg-clip-text">Selected works</div>
+        <div className="text-gray-500 text-primary">Glimpses into some of my projects</div>
+      </div>
       <div className="flex flex-col gap-5 bg-white h-auto p-8 rounded-2xl">
-        <div className="flex flex-col gap-4">
-          <div className="text-4xl font-semibold">Selected works</div>
-          <div className="text-gray-500">Glimpses into some of my projects</div>
-        </div>
-        <ProjectCrousel/>
+        <ProjectCrousel />
       </div>
     </div>
   );
